8024f0be1a7f064a2bf2342507407c44ff468f1a
Vinko Buble <vinkobuble@gmail.com>
2024-07-04 21:42:35 -0400

    retry policy and dead-letter queue

--- worker/src/handler.py
+++ worker/src/handler.py
@@ -8,10 +8,10 @@ from worker_contract import MultiplyMatricesWorkerRequest
 logger = logging.getLogger(__name__)
 
 
-def on_message(chan, method_frame, header_frame, body, userdata=None):
+def on_message(channel, method_frame, header_frame, body, userdata=None):
     """Called when a message is received. Log message and ack it."""
     logger.info('Received matrix multiplication job: %s', body)
     request = MultiplyMatricesWorkerRequest.model_validate(json.loads(body))
     result = np.dot(request.a.data, request.b.data)
     logger.info("Result stored in the log: %s", json.dumps(result.tolist(), indent=2))
-    chan.basic_ack(delivery_tag=method_frame.delivery_tag)
+    channel.basic_ack(delivery_tag=method_frame.delivery_tag)
